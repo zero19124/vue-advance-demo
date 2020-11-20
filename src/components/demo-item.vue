2adebaba97b6d4b0a2a4e92b199018dc465b5df6
jack <jack@cititrans.com>
2020-11-20 11:33:32 +0800

    vue $ref $parent $children 练习

---
+++ src/components/demo-item.vue
@@ -0,0 +1,36 @@
+<template>
+	<div>
+
+		<h2>{{header}}</h2>
+
+		<input type="text" class="form-control" v-model="val"
+		ref="input" @input="changeInput"/>
+
+	</div>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				header:"标题",
+				val: '输入框'
+			}
+		},
+		methods: {
+			focus() {
+				this.$refs.input.focus()
+			},
+			renameHeader(data){
+				this.header = data
+			},
+			changeInput(e){
+				this.$parent.header = e.target.value
+				this.$parent.parentRename(e.target.value)
+			}
+		},
+	}
+</script>
+
+<style>
+</style>
